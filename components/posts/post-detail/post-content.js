2916ef7047375493c3b7a1fb099ed5b34f6d7987
Benjaman Tran <t16021999@gmail.com>
2024-02-06 14:28:10 +0700

    config details page for code space with markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,13 +1,41 @@
 import PostHeader from './post-header.js'
 import classes from './post-content.module.css'
 import ReactMarkdown from 'react-markdown'
-
+import Image from 'next/image.js'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export default function PostContent(props) {
   const { post } = props
   const imagePath = `/images/posts/${post.slug}/${post.image}`
+  const customRenders = {
+    p(paragraph) {
+      const { node } = paragraph
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0]
+
+        return <div className={classes.image}>
+          <Image
+            src={`/images/posts/${post.slug}/${image.properties.src}`}
+            alt={image.properties.alt} width={600} height={300} />
+        </div>
+      }
+
+      return <p>{paragraph.children}</p>
+    },
+    code(code) {
+      const { className, children } = code
+      const language = className.split('-')[1]
+
+      return <SyntaxHighlighter
+        style={atomDark}
+        language={language}
+      >{children}</SyntaxHighlighter>
+    }
+  }
+
   return <article className={classes.content}>
     <PostHeader title={post.title} image={imagePath} />
-    <ReactMarkdown>{post.content}</ReactMarkdown>
+    <ReactMarkdown components={customRenders}>{post.content}</ReactMarkdown>
   </article>
 }

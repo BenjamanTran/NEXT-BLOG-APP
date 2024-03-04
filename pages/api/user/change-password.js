195c1d804ddd17bc3c4b8149e789179544c6735f
Benjaman Tran <t16021999@gmail.com>
2024-03-04 09:03:42 +0700

    delete console log

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -3,20 +3,17 @@ import { connectToDatabase } from "@/lib/db";
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]";
 
-
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
     return
   }
   const session = await getServerSession(req, res, authOptions)
-  console.log("user:", session)
   if (!session) {
     res.status(401).json({ message: 'Not authenticated!' })
     return
   }
 
   const userEmail = session.email
-  console.log("email:", userEmail)
   const oldPassword = req.body.oldPassword
   const newPassword = req.body.newPassword
 
